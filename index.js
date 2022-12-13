9f18576a41a8075d237c4b7a9caaa94daf21989a
Ragnar <ragnar.naruski@voco.ee>
2022-12-13 12:34:17 +0200

    add data json file and display it on view dynamically

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express=require('express')
 const app= express()
 
+//add template engine
 const path=require('path')
 app.set('view engine', 'ejs')
 app.set('views',path.join(__dirname,'views'))
+//static files css js
+app.use(express.static(path.join(__dirname,'public')))
+//use file system - read and write files
+const fs=require('fs')
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    fs.readFile('./tasks.json',"utf-8",(err,jsonstring)=>{
+        if (err){
+            console.log('Error reading file', err)
+            return
+        }
+        try{
+            const tasks=JSON.parse(jsonstring)
+            res.render('index',{tasksData:tasks})
+            console.log(tasks)
+        }catch (err){
+            console.log('Error parsing json file',err)
+        }
+    })
+
 })
 
 
